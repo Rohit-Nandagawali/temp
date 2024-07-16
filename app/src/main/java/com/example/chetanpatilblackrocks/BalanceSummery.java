952635a5d3f637dc17f9b6d43b1d933ad7a69812
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2024-07-16 23:55:33 +0530

    Bill Reminder added

---
+++ app/src/main/java/com/example/chetanpatilblackrocks/BalanceSummery.java
@@ -0,0 +1,14 @@
+package com.example.chetanpatilblackrocks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BalanceSummery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balance_summery);
+    }
+}
